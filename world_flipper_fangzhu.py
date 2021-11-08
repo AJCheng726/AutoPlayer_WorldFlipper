98833d814298fbeea3dd4de86fe51e9901999205
AJCheng726 <aj.cheng@foxmail.com>
2021-11-08 21:52:37 +0800

    1108

--- world_flipper_fangzhu.py
+++ world_flipper_fangzhu.py
@@ -2,6 +2,8 @@
 import datetime
 import time
 
+from numpy import _FlatIterSelf
+
 import auto_player as player
 from settings import *
 
@@ -9,21 +11,28 @@ from settings import *
 def wf_owner(use_device,loop_time = 0):
     player.adb_test()
     count = 0
+    timeout_flag = False
     print("[info] 使用设备{0}开始建房...".format(use_device))
     while count < loop_time or loop_time == 0:
         # 等待战斗中的暂停键,没出现就一直点挑战
         print("(stage1) 在房间中等待队友...")
         while not player.find("button_pause", device=use_device):
             player.wait_touch("button_tiaozhan", device=use_device, max_wait_time=5)
+            if player.find_touch("button_ok", device=use_device): # 超时房间解散
+                print("房间超时解散...准备重建...")
+                timeout_flag = True
+
         # 没有退出房间之前,无限尝试暂停=>放弃=>"是"
-        print("(stage2) 房主退出战斗中...")
-        while not player.find("button_duorenyouxi", device=use_device):
-            player.wait_touch("button_pause", device=use_device, max_wait_time=1)
-            player.wait_touch("button_fangqi", device=use_device, max_wait_time=1)
-            player.wait_touch("button_shi", device=use_device, max_wait_time=1)
-        if wait_outof_room > 0:
-            print("防止等待超时，等待{0}秒后重新建房...".format(wait_outof_room))
-            time.sleep(wait_outof_room)
+        if not timeout_flag:
+            print("(stage2) 房主退出战斗中...")
+            while not player.find("button_duorenyouxi", device=use_device):
+                player.wait_touch("button_pause", device=use_device, max_wait_time=1)
+                player.wait_touch("button_fangqi", device=use_device, max_wait_time=1)
+                player.wait_touch("button_shi", device=use_device, max_wait_time=1)
+            if wait_outof_room > 0:
+                print("防止等待超时，等待{0}秒后重新建房...".format(wait_outof_room))
+                time.sleep(wait_outof_room)
+                
         print("(stage3) 房主重建房...")
         player.wait_touch("button_duorenyouxi", device=use_device, max_wait_time=60)
         player.wait_touch("button_shi", device=use_device, max_wait_time=5)
