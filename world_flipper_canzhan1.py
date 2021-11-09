0bfb40191999916c33f54f34c385f77000b27f41
AJCheng726 <aj.cheng@foxmail.com>
2021-11-09 17:56:56 +0800

    1109

--- world_flipper_canzhan1.py
+++ world_flipper_canzhan1.py
@@ -13,16 +13,14 @@ def wf_join(use_device,loop_time = 0):
     while count < loop_time or loop_time == 0:
         # 战斗中=>继续（同时处理升级、掉落）=>离开房间
         print(datetime.datetime.now(),"(stage1) 等待战斗结算...")
-        player.wait("button_jixu", device=use_device, max_wait_time=600)
-        timer = Timer()
-        while not player.find("button_likaifangjian", device=use_device):
-            player.find_touch("button_jixu", device=use_device)
-            player.touch((device_w * 1 / 2, device_h * 1 / 2), device=use_device)
-            if timer.get_duration() > timeout:
-                print("超过{0}秒，可能阵亡未结算...".format(timeout))
-                player.find_touch("button_ok", device=use_device)
-                break
-        player.wait_touch("button_likaifangjian", device=use_device, max_wait_time=10)
+        if player.wait("button_jixu", device=use_device, max_wait_time=timeout):
+            while not player.find("button_likaifangjian", device=use_device):
+                player.find_touch("button_jixu", device=use_device)
+                player.touch((device_w * 1 / 2, device_h * 1 / 2), device=use_device)
+            player.wait_touch("button_likaifangjian", device=use_device, max_wait_time=10)
+        else:
+            print("超过{0}秒，可能阵亡未结算...".format(timeout))
+            player.find_touch("button_ok", device=use_device)
 
         # 找建房号ID=>"ok"和"是"处理双倍\房满的问题=>没找到就更新=>准备完毕
         print(datetime.datetime.now(),"(stage2) 再次寻找房间...")
