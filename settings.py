9bb8bf031c25a5cdec544ed7072e5a806fe78cae
AJCheng726 <aj.cheng@foxmail.com>
2021-11-24 23:46:12 +0800

    1124

--- settings.py
+++ settings.py
@@ -17,12 +17,12 @@ adb_path = "toolkits\\ADB\\adb.exe"
 wf_apk_name,wf_active_class_name = 'com.leiting.wf','air.com.leiting.wf.AppEntry'
 # ===单开/双开/三开刷共斗===
 # 蹭房设备号，如果是雷电64单开就填emulator-5554
-canzhan_device_1 = "emulator-5554"
+canzhan_device_1 = "emulator-5556"
 canzhan_device_2 = "emulator-5560"
 # 建房设备号，如果是雷电64多开，就按多开每次递增2
-fangzhu_device = "emulator-5556"
+fangzhu_device = "emulator-5554"
 # 房主ID用哪个，模拟器原比例截图放到wanted文件夹下，名字填在下面，按优先级从高到低找房
-fangzhu_account = ["ajcheng_pickup","ajcheng","azhecheng_pickup","azhecheng"]
+fangzhu_account = ["icon_chufaqianpickup","icon_chufaqian"]
 # 房间人数最小限制，已实现未测试
 limit_player = 2
 # 超时，超过则认为阵亡或房间解散
@@ -32,8 +32,8 @@ battle_timeout = 300
 event_mode = 0
 event_screenshot = "raid_event1"
 # ===日常模式====
-raid_choose = "raid_dashe"
-# raid_choose = "raid_pickup"
+# raid_choose = "raid_dashe"
+raid_choose = "raid_pickup"
 # ===单机连战===
 # 单人连战设备号
 loop_device = "emulator-5554"
