e8644ae72d91ee60dc65bffe9f6153125eac2df5
AJCheng726 <aj.cheng@foxmail.com>
2022-03-16 23:28:14 +0800

    v1.17.1
    新增自动更新

--- utils/update.py
+++ utils/update.py
@@ -10,13 +10,13 @@ from utils.print_color import printGreen, printPink, printRed
 
 timer = Timer.Timer()
 try:
-    printPink("{0} ApWF正在从git更新...".format(timer.simple_time()))
+    printPink("{0} ApWF正在从git更新...第一次更新根据网络情况需要10-20分钟".format(timer.simple_time()))
     repo = git.Repo("./")
     remote = repo.remote()
     info = remote.pull()
     for i in info:
         print(i)
-    printGreen("更新成功")
+    printGreen("{0} 更新成功".format(timer.simple_time()))
 except Exception as e:
     printRed(e)
-    printRed("更新失败，尝试前往 https://github.com/AJCheng726/AutoPlayer_WorldFlipper 手动更新")
+    printRed("{0} 更新失败，尝试前往 https://github.com/AJCheng726/AutoPlayer_WorldFlipper 手动更新".format(timer.simple_time()))
