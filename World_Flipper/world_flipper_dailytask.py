687d2bcf513718f4d5c360fc1c99eba641b85096
AJCheng726 <aj.cheng@foxmail.com>
2022-04-21 19:52:06 +0800

    Update world_flipper_dailytask.py

--- World_Flipper/world_flipper_dailytask.py
+++ World_Flipper/world_flipper_dailytask.py
@@ -52,7 +52,7 @@ def maze_repeat(player, maze_choise="maze_fire", repeat=4):
 def daily_task(player, maze_choise="maze_fire", repeat=4):
     if not check_game(player):
         login(player)
-    # buy_zhenqipin(player)
+    buy_zhenqipin(player)
     maze_repeat(player, maze_choise=maze_choise, repeat=repeat)
     printBlue("{0} 完成每日任务，返回主城".format(player.use_device))
 
